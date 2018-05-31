0c7d3c41d64c895dc41e4d71885c491f92530d9f
ngt-kn <nagatak@Dev.local>
2018-05-30 20:14:02 -0700

    implement image picker

--- ChatApp/Controllers/SelectPictureViewController.swift
+++ ChatApp/Controllers/SelectPictureViewController.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 
-class SelectPictureViewController: UIViewController {
+class SelectPictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var messageTextField: UITextField!
+    
+    var imagePicker : UIImagePickerController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker = UIImagePickerController()
+        imagePicker?.delegate = self
     }
 
 
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        if imagePicker != nil {
+            imagePicker?.sourceType = .camera
+            present(imagePicker!, animated: true, completion: nil)
+        }
     }
     @IBAction func selectPhotoPressed(_ sender: UIBarButtonItem) {
+        if imagePicker != nil {
+            imagePicker?.sourceType = .photoLibrary
+            present(imagePicker!, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
     @IBAction func nextButtonPressed(_ sender: UIButton) {
     }
